08970311e4d5c11d58dc4ceb56d2a5beeab1c533
LontchiFrank <fodjof6@gmail.com>
2024-03-03 22:35:11 +0100

    cone

--- config/database.js
+++ config/database.js
@@ -2,16 +2,24 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
+// const TodoDB = async () => {
+//   try {
+//     await mongoose.connect(db, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Succesfully connection to the database");
+//   } catch (err) {
+//     console.log(err.message);
+//     console.log("error");
+//   }
+// };
 const TodoDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Succesfully connection to the database");
-  } catch (err) {
-    console.log(err.message);
-    console.log("error");
+    await mongoose.connect(db, {});
+    console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+  } catch (error) {
+    console.error("COULD NOT CONNECT TO DATABASE:", error.message);
   }
 };
 
